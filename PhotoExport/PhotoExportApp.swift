ad7287a4e74599a01b0b99b5ec5812633d0faa82
David Mytton <david@mytton.net>
2025-02-16 15:07:13 +0000

    Initial Commit

---
+++ PhotoExport/PhotoExportApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoExportApp.swift
+//  PhotoExport
+//
+//  Created by David Mytton on 2025-02-16.
+//
+
+import SwiftUI
+
+@main
+struct PhotoExportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PhotoExportTests/PhotoExportTests.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoExportTests.swift
+//  PhotoExportTests
+//
+//  Created by David Mytton on 2025-02-16.
+//
+
+import Testing
+@testable import PhotoExport
+
+struct PhotoExportTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
